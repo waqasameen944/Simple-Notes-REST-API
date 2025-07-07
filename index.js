e8200cd23beeee740afd60f226605a15842be724
WAQAS <waqasameen944@gmail.com>
2025-07-07 20:35:34 +0500

    done home router

--- index.js
+++ index.js
@@ -16,14 +16,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 //routes
-app.get("/", (req, res) => {
-  res.send("hello world");
-});
-
 app.use("/api/v1/notes", NotesRoutes);
 
 //error handler
 app.use(errorMiddleware);
+
 //server
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`server running on port ${PORT}`));
